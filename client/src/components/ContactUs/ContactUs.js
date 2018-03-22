f98acd3f79a416fef9ab335342a1e5077e74357e
Mi Hyun An <mihyunan@usc.edu>
2018-03-22 14:54:08 -0700

    Revert "Contactus"

--- client/src/components/ContactUs/ContactUs.js
+++ client/src/components/ContactUs/ContactUs.js
@@ -1,75 +1,50 @@
-import React, { Component } from "react";
-import MenuBar from "../Home/MenuBar.js";
-import "../Signup/css/Signup.css";
+import React, { Component } from 'react';
+import MenuBar from '../Home/MenuBar.js'
+import '../Signup/Signup.css';
 
 export default class ContactUs extends Component {
-  componentDidMount() {
-    document.title = "Contact Us - ASIE Resume Database";
+ componentDidMount(){
+    document.title = "Contact Us - ASIE Resume Database"
   }
 
   render() {
     return (
-      <div>
-        <MenuBar />
-        <div className="container">
-          <div className="row">
-            <div className="col-6 form-box ">
-              <form className="signup-form=" action="/contactus" method="post">
-                <div className="form-group">
-                  <h1>Contact Us</h1>
+        <div>
+           <MenuBar/>
 
-                  <div>
-                    <label>First Name</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      name="firstName"
-                    />
-                  </div>
-                  <div>
-                    <label>Last Name</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      name="lastName"
-                    />
-                  </div>
-                  <div>
-                    <label>Email</label>
-                    <input className="form-control" type="text" name="email" />
-                  </div>
-                  <div>
-                    <label>Subject</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      name="subject"
-                    />
-                  </div>
-                  <div>
-                    <label>Message</label>
-                    <textarea
-                      className="form-control"
-                      rows="5"
-                      name="message"
-                    />
-                  </div>
-                  <div>
-                    <input
-                      className="btn btn-primary"
-                      type="submit"
-                      value="Submit"
-                    />
-                  </div>
-                </div>
-              </form>
-            </div>{" "}
-            {/*col-8*/}
-          </div>{" "}
-          {/*row*/}
-        </div>{" "}
-        {/*container*/}
-      </div> /*Empty div to wrap JSX*/
+        <div className="container">
+        <div className="row">   
+        <div className="col-6 form-box">
+     
+          <form className="signup-form" action="" method="">
+            <div className="form-group col-sm-10 col-md-8 col-lg-6 form-box">
+              <h1>Contact Us</h1>
+              
+              <div>
+                <label>Name</label>
+                <input className="form-control" type="text" name="name"/>
+              </div>
+              <div>
+                <label>Email</label>
+                <input className="form-control" type="text" name="email"/>
+              </div>
+              <div>
+                <label>Subject</label>
+                <input className="form-control" type="text" name="subject"/>
+              </div>
+              <div>
+                <label>Message</label>
+                <textarea className="form-control" rows="5" name="message"/>
+              </div>
+              <div>
+                <input className="btn btn-primary" type="submit" value="Submit"/>
+              </div>
+            </div>
+            </form>
+        </div> {/*col-8*/}
+        </div> {/*row*/}
+        </div> {/*container*/}
+        </div> /*Empty div to wrap JSX*/
     );
   }
 }
--- server.js
+++ server.js
@@ -1,63 +1,54 @@
-let express = require("express"),
-  proxy = require("express-http-proxy"),
-  session = require("express-session"),
-  cookieParser = require("cookie-parser"),
-  morgan = require("morgan"),
-  multer = require("multer"),
-  flash = require("connect-flash"),
-  bodyParser = require("body-parser"),
-  dbinit = require("./db/dbinit"),
-  passport = require("passport"),
-  LocalStrategy = require("passport-local").Strategy,
-  bcrypt = require("bcrypt"),
-  Model = require("./db/Model/Models"),
-  User = require("./db/Model/User"),
-  Employer = require("./db/Model/Employer"),
-  Candidate = require("./db/Model/Candidate"),
-  Transportation = require("./db/Model/Transportation"),
-  Hours = require("./db/Model/Hours"),
-  Skills = require("./db/Model/Skills"),
-  Experience = require("./db/Model/Experience"),
-  Interest = require("./db/Model/Interest"),
-  Portfolio = require("./db/Model/Portfolio"),
-  SearchCandidates = require("./db/searchcandidates"),
-  SearchEmployers = require("./db/searchemployers"),
-  db = require("./db/db"),
-  // delete this after dev
-  user_resp = require("./profile.js"),
-  app = express();
+let express = require('express'),
+    proxy = require('express-http-proxy'),
+    session = require('express-session'),
+    cookieParser = require('cookie-parser'),
+    morgan = require('morgan'),
+    multer = require('multer'),
+    flash = require('connect-flash'),
+    bodyParser = require('body-parser'),
+    dbinit = require('./db/dbinit'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    bcrypt = require('bcrypt'),
+    Model = require('./db/Model/Models'),
+    User = require('./db/Model/User'),
+    Candidate = require('./db/Model/Candidate'),
+    Transportation = require('./db/Model/Transportation'),
+    Hours = require('./db/Model/Hours'),
+    Skills = require('./db/Model/Skills'),
+    Experience = require('./db/Model/Experience'),
+    Interest = require('./db/Model/Interest'),
+    Portfolio = require('./db/Model/Portfolio'),
+    SearchCandidates = require('./db/searchcandidates'),
+    db = require('./db/db'),
+    // delete this after dev
+    user_resp = require('./profile.js'),
+    app = express();
 
 app.use(cookieParser());
-app.use(
-  session({
-    secret: "gravy sauce",
-    resave: true,
-    saveUninitialized: true,
-    proxy: true
-  })
-);
+app.use(session({secret: "gravy sauce", resave: true, saveUninitialized: true, proxy: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 const port = process.env.PORT || 3001;
 let portfolio_storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, "client/public/portfolio/");
+  destination: function (req, file, cb) {
+    cb(null, 'client/public/portfolio/')
   },
-  filename: function(req, file, cb) {
-    cb(null, Date.now() + ".jpg"); //Appending .jpg
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '.jpg') //Appending .jpg
   }
 });
 let portfolio_upload = multer({ storage: portfolio_storage });
 let profile_storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, "client/public/profile/");
+  destination: function (req, file, cb) {
+    cb(null, 'client/public/profile/')
   },
-  filename: function(req, file, cb) {
-    cb(null, Date.now() + ".jpg"); //Appending .jpg
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '.jpg') //Appending .jpg
   }
 });
 let profile_update = multer({ storage: profile_storage });
@@ -71,126 +62,104 @@ function ensureAuthenticated(req, res, next) {
 
   // denied. redirect to login
   console.log("not authorized", req.originalUrl);
-  res.redirect("/login");
+  res.redirect('/login');
 }
 
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: "email",
-      passwordField: "password"
-    },
-    function(email, password, done) {
-      User.findByEP(email, password, function(user) {
-        if (user == null) {
-          console.log("logged in failed - user");
-          return done(null, false, { message: "Incorrect credentials." });
-        }
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+  function(email, password, done) {
+    User.findByEP(email, password, function(user) {
+      if(user == null) {
+        console.log("logged in failed - user");
+        return done(null, false, { message: 'Incorrect credentials.' })
+      }
 
-        console.log("logged in successful");
-        return done(null, user);
-      });
-    }
-  )
-);
+      console.log("logged in successful");
+      return done(null, user)
+    })
+  }
+))
 
 passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
+  done(null, user.id)
+})
 
 passport.deserializeUser(function(id, done) {
   User.findById(id, function(user) {
     if (user == null) {
-      done(new Error("Wrong user id."));
+      done(new Error('Wrong user id.'))
     }
     //console.log("des", user);
-    done(null, user);
+    done(null, user)
   });
-});
+})
 
-app.post("/signup", Candidate.createProfile, (req, res) => {
-  res.redirect("/login");
-});
+app.post('/signup',
+  Candidate.createProfile,
+  (req, res) => {
+    res.redirect('/login');
+  });
 
-app.post("/contactus", Employer.createEmployer, (req, res) => {
-  res.redirect("/");
-});
-
-app.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/candidate",
-    failureRedirect: "/login",
-    failureFlash: true
-  }),
-  function(req, res) {}
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/candidate',
+                                   failureRedirect: '/login',
+                                   failureFlash: true }), function(req, res) {
+  }
 );
-app.get("/logout", function(req, res) {
-  console.log("logging out");
+app.get('/logout', function(req, res){
+  console.log('logging out');
   req.logout();
-  res.redirect("/");
+  res.redirect('/');
 });
-app.get("/protected", ensureAuthenticated, function(req, res) {
+app.get('/protected', ensureAuthenticated, function(req, res) {
   res.send("access granted. secure stuff happens here " + req.user.id);
 });
 
-app.get("/api/hello", (req, res) => {
-  res.send({ express: "Hello From Express" });
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
 });
 
 //app.get('/api/candidate', User.findByIdRes);
-app.get("/api/candidate", (req, res) => {
+app.get('/api/candidate', (req, res) => {
   res.json(user_resp.sample_user);
 });
-app.get("/api/candidate/profile", Candidate.getProfileRes);
-app.get("/api/candidate/experience", Experience.getRes);
-app.get("/api/candidate/education", (req, res) => {
+app.get('/api/candidate/profile', Candidate.getProfileRes);
+app.get('/api/candidate/experience', Experience.getRes);
+app.get('/api/candidate/education', (req, res) => {
   res.json(user_resp.education);
 });
-app.get("/api/candidate/skills", Skills.getRes);
-app.get("/api/candidate/interest", Interest.getRes);
-app.get("/api/candidate/hours", Hours.getRes);
-app.get("/api/candidate/transportation", Transportation.getRes);
-app.get("/api/candidate/portfolio", Portfolio.getRes);
+app.get('/api/candidate/skills', Skills.getRes);
+app.get('/api/candidate/interest', Interest.getRes);
+app.get('/api/candidate/hours', Hours.getRes);
+app.get('/api/candidate/transportation', Transportation.getRes);
+app.get('/api/candidate/portfolio', Portfolio.getRes);
 
-app.post(
-  "/api/candidate/update/profile",
-  profile_update.single("update_image"),
-  Candidate.updateProfile
-);
-app.post(
-  "/api/candidate/update/experience",
-  portfolio_upload.array(),
-  Experience.update
-);
-app.post("/api/candidate/update/skills", Skills.update);
-app.post("/api/candidate/update/interest", Interest.update);
-app.post(
-  "/api/candidate/update/transportation",
-  portfolio_upload.array(),
-  Transportation.update
-);
-app.post("/api/candidate/update/hours", portfolio_upload.array(), Hours.update);
-app.post(
-  "/api/candidate/update/portfolio",
-  portfolio_upload.single("image"),
-  Portfolio.update
-);
 
-app.get("/api/search/candidate", SearchCandidates.search);
-app.get("/api/search/employers", SearchEmployers.search);
+app.post('/api/candidate/update/profile', profile_update.single("update_image"), Candidate.updateProfile);
+app.post('/api/candidate/update/experience', portfolio_upload.array(), Experience.update);
+app.post('/api/candidate/update/skills', Skills.update);
+app.post('/api/candidate/update/interest', Interest.update);
+app.post('/api/candidate/update/transportation', portfolio_upload.array(), Transportation.update);
+app.post('/api/candidate/update/hours', portfolio_upload.array(), Hours.update);
+app.post('/api/candidate/update/portfolio', portfolio_upload.single("image"), Portfolio.update);
 
-app.use(
-  "/candidate",
-  ensureAuthenticated,
-  proxy("http://127.0.0.1:3000/candidate")
-);
+app.get('/api/search/candidate', SearchCandidates.search);
 
-app.get("/robots.txt", function(req, res) {
-  res.type("text/plain");
-  res.send("User-agent: *\nDisallow: /");
+
+app.use('/candidate', ensureAuthenticated,
+                      proxy('http://127.0.0.1:3000/candidate'));
+
+
+app.get('/robots.txt', function(req, res) {
+  res.type('text/plain');
+  res.send('User-agent: *\nDisallow: /');
 });
 
-app.use("/", proxy("127.0.0.1:3000/"));
+app.use('/', proxy('127.0.0.1:3000/'));
+
+
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
