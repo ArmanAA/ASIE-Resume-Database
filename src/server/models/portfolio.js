a75d74e008962727fd39792684c94964ee9635fb
mihyunan <mihyunan@usc.edu>
2018-04-08 17:06:04 -0700

    removed thumbnail from portfolio model

--- src/server/models/portfolio.js
+++ src/server/models/portfolio.js
@@ -7,7 +7,6 @@ module.exports = (sequelize, DataTypes) => {
 		},
 		url: DataTypes.STRING,
 		title: DataTypes.STRING,
-		thumbnail: DataTypes.STRING,
 		description: DataTypes.STRING
 	}, {
 		omitNull: true
