c726f7e454594e7321a0689db2d89b92546b81b9
mihyunan <mihyunan@usc.edu>
2018-04-08 17:07:32 -0700

    updated portfolio router to add videos

--- src/server/routes/portfolios.js
+++ src/server/routes/portfolios.js
@@ -1,13 +1,17 @@
 let models  = require('../models'),
+	URL = require('url'),
 	multer = require('multer'),
 	express = require('express'),
 	router  = express.Router();
 
 let image_dir = "/portfolio/";
+let youtube_start = "https://img.youtube.com/vi/";
+let youtube_end = "/0.jpg";
+let youtube_url = "https://www.youtube.com/watch?v=";
 
 let portfolio_storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../../../public/portfolio/')
+    cb(null, __dirname + '/../../../public/portfolio/')
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '.jpg') //Appending .jpg
@@ -24,10 +28,12 @@ router.get('/:user_id', function(req, res) {
 	}).then((results) => {
 		let portfolio = [];
 		results.forEach((result) => {
+			let thumbnail = result.type == "picture" ? image_dir + result.url : youtube_start + result.url + youtube_end;
+			let url = results.type == "picture" ? image_dir + result.url : youtube_url + result.url;
 			let portfolio_elem = {
 				title: result.title,
-				url: image_dir + result.url,
-				thumbnail: image_dir + result.url,
+				url: url,
+				thumbnail: thumbnail,
 				description: result.description
 			};
 			portfolio.push(portfolio_elem);
@@ -38,10 +44,19 @@ router.get('/:user_id', function(req, res) {
 
 router.post('/:user_id/update', portfolio_upload.single("image"), function(req, res) {
 	let action = 'add';//req.body.action;
-	let type = 'picture';//req.body.type;
-	let url = req.file.filename;//req.body.url;
+	
+	if(req.file) {
+		var url = req.file.filename;//req.body.url;
+		var type = 'picture';//req.body.type;
+	}
+	else {
+		var type = 'video';//req.body.type;
+		let url_raw = req.body.video;
+		let link = URL.parse(url_raw, true);
+		let query = link.query;
+		var url = query.v;
+	}
 	let title = req.body.title;
-	let thumbnail = req.file.filename;
 	let description = req.body.description;
 
 	if(action == "add") {
@@ -50,7 +65,6 @@ router.post('/:user_id/update', portfolio_upload.single("image"), function(req, 
 			type: type,
 			url: url,
 			title: title,
-			thumbnail: thumbnail,
 			description: description
 		}).then(results => {
 			res.json({message: 'successful'})
