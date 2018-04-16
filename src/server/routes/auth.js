6e389206d4e423db1589137ad0d4983f93918066
Jason Kwak <jasonkkw@usc.edu>
2018-04-16 00:00:04 -0700

    Validation checks for auth.js
    Checks if user is logged in

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -1,23 +1,26 @@
 module.exports.admin = (req, res, next) => {
+	console.log("USER", req.user);
 	if(!req.isAuthenticated()) {
 		// denied. redirect to login
 		console.log("not authorized as ADMIN", req.originalUrl);
 		res.redirect("/login");
 	}
-
-	let usertype = req.user.usertype || "";
-	let is_admin = ['SUPER', 'ADMIN', 'FAC'].indexOf(usertype) > -1;
-	//console.log("AUTH", req.user);
-	//ADMIN auth given to FAC for dev; Change to ADMIN later
-	if (is_admin) {
-		// req.user is available for use here
-		console.log("authorized ADMIN", req.originalUrl);
-		return next();
+	else {
+		let usertype = req.user.usertype || "";
+		let is_admin = ['SUPER', 'ADMIN', 'FAC'].indexOf(usertype) > -1;
+		//console.log("AUTH", req.user);
+		//ADMIN auth given to FAC for dev; Change to ADMIN later
+		if (is_admin) {
+			// req.user is available for use here
+			console.log("authorized ADMIN", req.originalUrl);
+			return next();
+		}
+		else {
+			// denied. redirect to login
+			console.log("not authorized as ADMIN", req.originalUrl);
+			res.redirect("/login");
+		}
 	}
-
-	// denied. redirect to login
-	console.log("not authorized as ADMIN", req.originalUrl);
-	res.redirect("/login");
 }
 
 module.exports.user = (req, res, next) => {
